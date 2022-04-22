56014eec739c150c4e203ed2832921293ec4bba2
Troy Krupinski <troy.krupinski@g.austincc.edu>
2022-04-22 10:17:56 -0500

    submitting styling and html

---
+++ application/views/pages/Final.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>GifTastic!</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  
+  <link href = "Assets/css/style.css" rel = "stylesheet">
+ <script src="Assets/javascript/fscript.js"></script>
+ 
+</head>
+
+<body>
+  <div id = "gifbuttons">
+
+  </div>
+  <div class="container">
+    <div class = "float-right">
+    <h1>Gif Search</h1>   
+    <h2>Click on gifs to back them start, search for gifs and it adds a button!</h2>
+
+ 
+    <form id="Gif-form">
+      <label for="gif-input">Search for a Gif! Add \</label>
+      <input type="text" id="gif-input"><br>
+
+
+      <input id="find-gif" type="submit" value="Search">
+    </form>
+    <div class = "title">
+
+    </div>
+    <div class = "id">
+
+    </div>
+    <div class = "source">
+
+    </div>
+  </div>
+</div>
+<div class = "float-left">
+    <div class = "container">
+        <div class = "row">
+   
+  </div>
+  </div>
+</div>
+   
+  
+</body>
+
+</html>
+
